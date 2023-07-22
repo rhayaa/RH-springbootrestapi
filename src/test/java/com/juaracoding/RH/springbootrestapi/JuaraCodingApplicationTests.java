0d3be59359f09cd80a7a3c37733c289dfa5ca3c7
Rhay <riswanhadi91@gmai.com>
2023-07-22 10:40:14 +0700

    commit project springboot

---
+++ src/test/java/com/juaracoding/RH/springbootrestapi/JuaraCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.RH.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuaraCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
